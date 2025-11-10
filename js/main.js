64b5a41640c53ec532f722f2ce07ec56b568beb8
Made0003 <made0003@algonquinlive.com>
2025-11-09 23:57:23 -0500

    adding dom event listener function init and click  event to generate number

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 / Wait for the DOM to be ready
 document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    // Your initialization code - click listener
+    console.log('DOM is ready. Initialization complete.');
+}
+
+/**
+ *  handle  click event                 
+ */
+function handleClick(ev) {
+    // random number generator
+    console.log('Body was clicked!');
+    
